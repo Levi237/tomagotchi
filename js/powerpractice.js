b02214f56d46b9393b1abbd7d26ab6c3f011cc67
Levi Winans <levieiko@gmail.com>
2019-04-03 11:48:32 -0700

    move interval into startGame function

--- js/powerpractice.js
+++ js/powerpractice.js
@@ -8,30 +8,20 @@ class Pet {
         this.tired = 0;
         this.bored = 0;
     }
-    // feed(){
-    //     // listen for click to give food to T
-    //     document.querySelector("feed")  // later add different foods
-    // }
-    // play(){
-    //     // listen for click to play with T
-    //     document.querySelector("play")
-    // }
-    // rest(){
-    //     // listen for click to rest T
-    //     document.querySelector("rest")
-    // }
+    // feed(){}
+    // play(){}
+    // rest(){}
 }
 
+//--> Variables
 let myPet = {};
 let age;
 let bored;
 let hunger;
 let tired;
 
-
+//--> Const / Functions
 const actionimage = document.querySelector("#image");
-// const prompt = document.querySelector("#prompt");
-
  
 const hungryBar = () => {
     let barHungry = document.querySelector("#hungry-bar")
@@ -59,13 +49,41 @@ const removePrompt = () => {
     e.parentNode.removeChild(e);
 }
 
-const gameOver = () => {
+const startGame = () => {
+    ageInter = setInterval(() => {
+        myPet.age++
+        document.querySelector("#age-ticker").innerHTML = `<b>${myPet.name}</b> is ${myPet.age} toma years old!`;
+        if (myPet.age === 5){
+            alert("congrats, your pet can evolve")
+        }}, 6000)
+    bored = setInterval(() => {
+        myPet.bored++
+        boredBar()
+        if (myPet.bored === 10){ 
+            gameOver();
+        }}, 800);
+    hunger = setInterval(() => {
+        myPet.hunger++
+        hungryBar()                               
+        if (myPet.hunger === 10){ 
+            gameOver()
+        }}, 1600);
+    tired = setInterval(() => {
+        myPet.tired++
+        tiredBar()
+        if (myPet.tired === 10){ 
+            gameOver()
+        }}, 2400);    
+}
+
+const gameOver = () => {   
     if (myPet.hunger === 10 || myPet.tired === 10 || myPet.bored === 10){
     clearInterval(bored);
     clearInterval(tired);
     clearInterval(hunger);
     clearInterval(ageInter);
     actionimage.src = "https://media.giphy.com/media/ktvFa67wmjDEI/giphy.gif";
+    // document.getElementsByTagName("button").removeEventListener();
     document.querySelector("#age-ticker").innerHTML = `<b>${myPet.name}</b> is dead at age ${myPet.age}`;}
 }
 
@@ -83,44 +101,48 @@ document.querySelector('#play').addEventListener('click', () => {
 }) 
 
 document.querySelector('#rest').addEventListener('click', () => {
+    // if (myPet.tired < 10) {
     myPet.tired--;
     actionimage.src = "https://media.giphy.com/media/31AFIhDmJA86Y/giphy.gif";
     tiredBar()
 })
+
+//--> EVOLVE ATTEMPT
+// https://media.giphy.com/media/hFqSV9JBsFbOw/giphy.gif
 
 //----> SUBMIT CLICK
 document.querySelector(".submitBtn").addEventListener('click', () => {
     const petName = document.querySelector('#submitScreen').value
     myPet = new Pet(petName);
     document.querySelector("#age-ticker").innerHTML = `<b>${myPet.name}</b> is ${myPet.age} toma years old!`;
-    actionimage.src = "https://media.giphy.com/media/IMfi5ugpf3W1y/giphy.gif";//https://media.giphy.com/media/IMfi5ugpf3W1y/giphy.gif
+    actionimage.src = "https://media.giphy.com/media/IMfi5ugpf3W1y/giphy.gif";
     removePrompt();
-    //--> INTERVAL TIMER
-        ageInter = setInterval(() => {
-            myPet.age++
-            document.querySelector("#age-ticker").innerHTML = `<b>${myPet.name}</b> is ${myPet.age} toma years old!`;
-            if (myPet.age === 5){
-                alert("congrats, your pet can evolve")
-            }}, 6000)
-        bored = setInterval(() => {
-            myPet.bored++
-            boredBar()
-            if (myPet.bored === 10){ 
-                gameOver();
-            }}, 800);
-        hunger = setInterval(() => {
-            myPet.hunger++
-            hungryBar()                               
-            if (myPet.hunger === 10){ 
-                gameOver()
-            }}, 1600);    
-        tired = setInterval(() => {
-            myPet.tired++
-            tiredBar()
-            if (myPet.hunger === 10 || myPet.tired === 10 || myPet.bored === 10){ 
-                clearInterval(tired)
-                gameOver()
-            }}, 2400);    
+    startGame();
+//--> INTERVAL TIMER
+        // ageInter = setInterval(() => {
+        //     myPet.age++
+        //     document.querySelector("#age-ticker").innerHTML = `<b>${myPet.name}</b> is ${myPet.age} toma years old!`;
+        //     if (myPet.age === 5){
+        //         alert("congrats, your pet can evolve")
+        //     }}, 6000)
+        // bored = setInterval(() => {
+        //     myPet.bored++
+        //     boredBar()
+        //     if (myPet.bored === 10){ 
+        //         gameOver();
+        //     }}, 800);
+        // hunger = setInterval(() => {
+        //     myPet.hunger++
+        //     hungryBar()                               
+        //     if (myPet.hunger === 10){ 
+        //         gameOver()
+        //     }}, 1600);
+        // tired = setInterval(() => {
+        //     myPet.tired++
+        //     tiredBar()
+        //     if (myPet.tired === 10){ 
+        //         gameOver()
+        //     }}, 2400);    
 })
 
 
